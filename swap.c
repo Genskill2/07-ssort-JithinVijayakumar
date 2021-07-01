d0e64a529fbfbc92ebae6209277ab683b6115b83
JithinVijayakumar <84662663+JithinVijayakumar@users.noreply.github.com>
2021-07-01 18:03:43 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,16 @@
+void swap_max(int a[], int len, int pos)
+  {
+  int max=a[pos],maxp=pos,t=0;
+  for(int i=pos;i<len;i++)
+  {
+  if (a[i]>max){
+  max=a[i];
+  maxp=i;
+  }
+  }
+  t=a[maxp];
+  a[maxp]=a[pos];
+  a[pos]=t;
+  
+  
+  }
